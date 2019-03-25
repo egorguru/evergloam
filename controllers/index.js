58d8566af517509ac3aa44205dab9c811203858a
EgorTrevelyan <egorepnikov@gmail.com>
2019-03-25 19:33:00 +0300

    Users controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const Router = require('koa-router')
 
 const auth = require('./auth')
+const users = require('./users')
 
 const router = new Router().prefix('/api')
 
-router.use(auth)
+router.use(auth, users)
 
 module.exports = router
---
+++ controllers/users.js
@@ -0,0 +1,17 @@
+const Router = require('koa-router')
+
+const User = require('../models/User')
+
+const router = new Router().prefix('/users')
+
+router.get('/:_id', async (ctx) => {
+  const { _id } = ctx.params
+  const user = await User.findById(_id)
+  if (user) {
+    ctx.body = user
+  } else {
+    ctx.throw(404)
+  }
+})
+
+module.exports = router.routes()
