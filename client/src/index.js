130d6f4eaab49102ec23f34e593c05d4fa9d1a6d
EgorTrevelyan <egorepnikov@gmail.com>
2019-03-31 15:50:03 +0300

    Add dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'font-awesome/css/font-awesome.min.css'
+import 'react-quill/dist/quill.snow.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
