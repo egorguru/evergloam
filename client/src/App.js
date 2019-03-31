caf97d31e2a4aedade55c7dca1673575dbc7e40f
EgorTrevelyan <egorepnikov@gmail.com>
2019-03-31 20:38:23 +0300

    Base structure of App and Redux support

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import store from './store'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App"></div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <React.Fragment>
+            <div className="container"></div>
+          </React.Fragment>
+        </BrowserRouter>
+      </Provider>
     )
   }
 }
