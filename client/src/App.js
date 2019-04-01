2175305977e7ffbb4f302bc642e79205d5aecf9d
EgorTrevelyan <egorepnikov@gmail.com>
2019-04-01 14:29:23 +0300

    Set Auth User on start up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import jwtDecode from 'jwt-decode'
 
 import store from './store'
+import setAuthToken from './utils/setAuthToken'
+
+import { setCurrentUser, logout } from './actions/authActions'
 
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import NotFound from './components/not-found/NotFound'
+
+if (localStorage.access_token) {
+  const { access_token } = localStorage
+  setAuthToken(access_token)
+  const decoded = jwtDecode(access_token)
+  store.dispatch(setCurrentUser(decoded))
+  const currentTime = Date.now() / 1000
+  if (decoded.exp < currentTime) {
+    store.dispatch(logout())
+    window.location.href = '/login'
+  }
+}
 
 class App extends React.Component {
   render() {
