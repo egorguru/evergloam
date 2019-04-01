eb89c348459ededc758c5ea1383180165bde00f3
EgorTrevelyan <egorepnikov@gmail.com>
2019-04-01 15:11:14 +0300

    Post and Like component

---
+++ client/src/components/shared/Post/Like.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
+import { createLike, removeLike } from '../../../actions/postActions'
+
+class Like extends React.Component {
+
+  onLikeClick(e) {
+    e.preventDefault()
+    const { auth, postId, likes, TYPE } = this.props
+    if (auth.isAuthenticated) {
+      const existedLike = likes.find((l) => l.user === auth.user.id)
+      if (existedLike) {
+        this.props.removeLike(postId, existedLike._id, TYPE)
+      } else {
+        this.props.createLike(postId, TYPE)
+      }
+    }
+  }
+
+  render() {
+    const { likes } = this.props
+    return (
+      <a
+        href="#" role="button"
+        className="card-link" onClick={this.onLikeClick.bind(this)}
+      >
+        <i className="fa fa-heart"></i> {likes.length}
+      </a>
+    )
+  }
+}
+
+Like.propTypes = {
+  createLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  postId: PropTypes.string.isRequired,
+  likes: PropTypes.array.isRequired,
+  TYPE: PropTypes.string.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { createLike, removeLike })(Like)
---
+++ client/src/components/shared/Post/Post.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import { remove } from '../../../actions/postActions'
+
+import Like from './Like'
+import ProfileImage from '../ProfileImage'
+
+class Post extends React.Component {
+
+  componentDidMount() {
+    this.refs.body.innerHTML = this.props.post.body
+  }
+
+  onDelete() {
+    this.props.remove(this.props.post._id)
+  }
+
+  render() {
+    const { post, auth, TYPE } = this.props
+    return (
+      <div className="card mb-4">
+        <div className="card-header">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="mr-2">
+                <Link to={'/user/' + post.user._id}>
+                  <ProfileImage user={post.user} width="50" />
+                </Link>
+              </div>
+              <div className="ml-2">
+                <div className="h5 m-0">{post.user.name}</div>
+                <div className="h7 text-muted">
+                  <i className="fa fa-clock-o"></i> {new Date(post.createdDate).toDateString()}
+                </div>
+              </div>
+            </div>
+            {auth.isAuthenticated && auth.user.name === post.user.name && (
+              <div className="dropdown">
+                <button className="btn btn-link dropdown-toggle" type="button" id="drop" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="drop">
+                  <a className="dropdown-item" role="button" onClick={this.onDelete.bind(this)}>Remove</a>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="card-body" ref="body"></div>
+        <div className="card-footer">
+          <Like postId={post._id} likes={post.likes} TYPE={TYPE} />
+          <Link to={'/post/' + post._id} className="card-link">
+            <i className="fa fa-arrow-right"></i>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+Post.propTypes = {
+  remove: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  TYPE: PropTypes.string.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { remove })(Post)
