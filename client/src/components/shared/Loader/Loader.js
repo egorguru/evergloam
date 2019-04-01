1ad53194fc3920e501f4305ff82c896a6d35072d
EgorTrevelyan <egorepnikov@gmail.com>
2019-04-01 15:16:41 +0300

    Loader component

---
+++ client/src/components/shared/Loader/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Loader.css'
+
+export default () => (
+  <div id="fountainG" className="mt-4">
+    <div id="fountainG_1" className="fountainG"></div>
+    <div id="fountainG_2" className="fountainG"></div>
+    <div id="fountainG_3" className="fountainG"></div>
+    <div id="fountainG_4" className="fountainG"></div>
+    <div id="fountainG_5" className="fountainG"></div>
+    <div id="fountainG_6" className="fountainG"></div>
+    <div id="fountainG_7" className="fountainG"></div>
+    <div id="fountainG_8" className="fountainG"></div>
+  </div>
+)
