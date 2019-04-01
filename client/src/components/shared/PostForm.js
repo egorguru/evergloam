6a266a84833abd590221aaf7712192d14166b903
EgorTrevelyan <egorepnikov@gmail.com>
2019-04-01 15:15:47 +0300

    PostFrom component

---
+++ client/src/components/shared/PostForm.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import Quill from 'react-quill'
+
+import { create } from '../../actions/postActions'
+
+class PostForm extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      body: ''
+    }
+    this.onChangeBody = this.onChangeBody.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChangeBody(body) {
+    this.setState({ body })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    this.props.create(this.state)
+    this.setState({ body: '' })
+  }
+
+  render() {
+    return (
+      <div className="card mb-4">
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <Quill
+                placeholder="What's up?"
+                theme="snow"
+                modules={{
+                  toolbar: [
+                    ['bold', 'italic', 'underline', 'strike'],
+                    ['link', 'image', 'video'],
+                    ['clean']
+                  ]
+                }}
+                value={this.state.body}
+                onChange={this.onChangeBody}
+              />
+            </div>
+            <div className="btn-group float-right">
+              <button type="submit" className="btn btn-dark">Share</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+PostForm.propTypes = {
+  create: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { create })(PostForm)
