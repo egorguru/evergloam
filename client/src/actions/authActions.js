4418b6933575a10de70da8d90b4ed57a7aa0d82e
EgorTrevelyan <egorepnikov@gmail.com>
2019-04-01 14:17:19 +0300

    Auth reducer-action layer

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
+
+import { SET_CURRENT_USER } from './types'
+import setAuthToken from '../utils/setAuthToken'
+
+export const register = (userData, history) => (dispatch) => {
+  axios
+    .post('/api/auth/register', userData)
+    .then((res) => history.push('/login'))
+}
+
+export const login = (userData) => (dispatch) => {
+  axios
+    .post('/api/auth/login', userData)
+    .then((res) => {
+      const { token } = res.data
+      localStorage.setItem('access_token', token)
+      setAuthToken(token)
+      const decoded = jwtDecode(token)
+      dispatch(setCurrentUser(decoded))
+    })
+}
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('access_token')
+  setAuthToken(false)
+  dispatch(setCurrentUser({}))
+}
+
+export const setCurrentUser = (user) => ({
+  type: SET_CURRENT_USER,
+  payload: user
+})
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,5 @@
 import { combineReducers } from 'redux'
 
-export default combineReducers({})
+import auth from './authReducer'
+
+export default combineReducers({ auth })
